1980889d0c0787bb1114f74c93c729b2be583e6d
hexirk <authirk@yandex.ru>
2022-11-09 21:19:46 +0800

    Лабораторная работа 1.1

---
+++ Sizykh_lab_1-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Sizykh_lab_1_1
+{
+    public class Logic
+    {
+        public static string IsLucky(int number)
+        {
+            string outMessage = "";
+            if (number >= 100000 && number <= 999999)
+            {
+                int d1 = number%1000000/100000;
+                int d2 = number%100000/10000;
+                int d3 = number%10000/1000;
+                int d4 = number%1000/100;
+                int d5 = number%100/10;
+                int d6 = number%10;
+                int sum1 = d1 + d2 + d3;
+                int sum2 = d4 + d5 + d6;
+                if (sum1 == sum2)
+                {
+                    outMessage = "Счастливое число";
+                }
+                else
+                {
+                    outMessage = "Обычное число";
+                }
+            }
+            else
+            {
+                outMessage = "Число не шестизначное";
+            }
+            return outMessage;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            var number = int.Parse(Console.ReadLine());
+            var outMessage = Logic.IsLucky(number);
+            Console.WriteLine(outMessage);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sizykh_lab_1-1Tests/LogicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sizykh_lab_1_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sizykh_lab_1_1.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void NumberIsNotLucky()
+        {
+            int number = 125186;
+            var message = Logic.IsLucky(number);
+            Assert.AreEqual("Обычное число", message);
+        }
+        [TestMethod()]
+        public void NumberIsLucky()
+        {
+            int number = 541235;
+            var message = Logic.IsLucky(number);
+            Assert.AreEqual("Счастливое число", message);
+        }
+        [TestMethod()]
+        public void NumberIsNot6digits()
+        {
+            int number = 123;
+            var message = Logic.IsLucky(number);
+            Assert.AreEqual("Число не шестизначное", message);
+        }
+    }
+}
